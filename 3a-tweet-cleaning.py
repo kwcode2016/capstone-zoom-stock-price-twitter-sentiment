8ffad632c3f8d8fcc4b3264e28339489d4b82e10
Ken Wee <kwhome2002@gmail.com>
2023-06-11 13:07:23 -0700

    completing 3a tweet cleaning

--- 3a-tweet-cleaning.py
+++ 3a-tweet-cleaning.py
@@ -1,6 +1,7 @@
 import re
 
 
+
 def remove_emoji(string):
     emoji_pattern = re.compile("["
                            u"\U0001F600-\U0001F64F" # emoticons
@@ -27,3 +28,36 @@ def remove_emoji(string):
 
 
 # unit testing
+tweet_sample_w_emojis = '''A Couple Morning Plays With One        🔥 📈 Running Over 60% 📈 🔥'''
+
+print(f'Original tweet with emojis: \t\t\t{tweet_sample_w_emojis}')
+print(f'Tweet after using remove_emoji function: \t{remove_emoji(tweet_sample_w_emojis)}')
+
+
+
+
+def remove_hyperlinks_twitter_marks (tweet):
+    # it will remove the old style retweet text "RT"
+    tweet2 = re.sub(r'^RT[\s]+', '', tweet)
+
+    # it will remove hyperlinks
+    tweet2 = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet2)
+
+    # it will remove hashtags. We have to be careful here not to remove 
+    # the whole hashtag because text of hashtags contains huge information. 
+    # only removing the hash # sign from the word
+    tweet2 = re.sub(r'#', '', tweet2)
+
+    # it will remove single numeric terms in the tweet. 
+    tweet2 = re.sub(r'[0-9]', '', tweet2)
+    return tweet2
+
+# unit testing 
+
+tweet_sample_w_links = 'Stats for the day have arrived. 1 new follower and NO unfollowers :) via http://t.co/0s8GQYOeus.'
+print()
+print(f'Original Tweet with hyperlinks and twitter marks: {tweet_sample_w_links}')
+print(f'Tweet after removing hyperlinks twitter marks: {remove_hyperlinks_twitter_marks(tweet_sample_w_links)}')
+
+
+
